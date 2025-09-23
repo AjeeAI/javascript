9e26ceae0f45f17675fc9ae4f29a0892722e6a71
Adesoji Ajijolaoluwa <ajeeaidev@gmail.com>
2025-09-23 14:29:50 +0100

    Javascript task for day 2 material. Task 1 - 4 done

---
+++ practice.js
@@ -0,0 +1,132 @@
+function sayHello(){
+    console.log("Hello from my first function!");
+}
+
+sayHello();
+console.log("")
+
+function showCurrentTime(){
+    let now = new Date();
+    console.log(`Current time: ${now.toLocaleTimeString()}`);
+}
+
+showCurrentTime();
+
+function greetPerson(name){
+    console.log(`Hello, ${name}! Welcome to Javascript!`)
+}
+
+greetPerson("Alice")
+greetPerson("Bob")
+greetPerson("Charlie")
+
+console.log("");
+
+function introduce(firstName, lastName, age){
+    console.log(`Hi! I'm ${firstName} ${lastName} and I'm ${age} years old.`)
+
+}
+
+introduce("Jane", "Smith", 25);
+introduce("John", "Doe", 30);
+introduce("Sarah", "Johnson", 22);
+
+console.log("");
+
+function greetWithDefault(name = "Friend"){
+    console.log(`Hello, ${name}`);
+}
+
+greetWithDefault("Maria");
+greetWithDefault();
+
+
+function addNumbers(num1, num2){
+    let result = num1 + num2;
+    return result;
+}
+
+let sum = addNumbers(5, 3);
+console.log("Sum:", sum);
+
+console.log("10 + 7 =", addNumbers(10, 7));
+
+console.log("");
+
+function createFullName(first, last){
+    return `${first} ${last}`.trim();
+}
+
+let fullName = createFullName("John", "Doe")
+console.log("Full name:", fullName);
+console.log("");
+
+
+function isAdult(age){
+    return age >= 18;
+}
+
+console.log("Is 16 adult?", isAdult(16));
+console.log("Is 21 adult?", isAdult(21));
+
+let userAge = 20;
+if (isAdult(userAge)){
+    console.log("User can vote!");
+} else{
+    console.log("User cannot vote yet.")
+}
+
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a ,b){
+    if (b === 0){
+        return "Cannot divide by Zero!";
+    }
+    return a / b;
+}
+ 
+function calculateArea(length, width){
+    return length * width;
+}
+
+console.log("Multiply 6 x 4 =", multiply(6, 4));
+console.log("Divide 15 by 3 =", divide(15, 3));
+console.log("Divide 15 by 0 = ", divide(15, 0));
+console.log("Area of 5 x 3 room =", calculateArea(5, 3), "square units");
+
+let age = 25;
+let temperature = 75;
+let isRaining = false;
+
+if (age >= 18){
+    console.log("You are an adult!");
+} 
+
+if (temperature > 80){
+    console.log("It's hot today!");
+} else {
+    console.log("The weather is nice!")
+}
+
+if (isRaining){
+    console.log("Don't forget your umbrella!");
+} else{
+    console.log("No umbrella needed today!")
+}
+
+console.log("")
+
+
+let score = 85;
+let passingGrade = 70;
+
+if (score >= passingGrade){
+    console.log(`Congratulations! You passed with ${score}%`);
+
+} else{
+    console.log(`Sorry, you scored ${score}%. You need ${passingGrade}% to pass.`)
+}
+
---
+++ task3.js
@@ -0,0 +1,153 @@
+//  Simple grading system
+
+function getGrade(score){
+    if (score === 100){
+        console.log(`Outstanding! Grade: A+`);
+    }
+    else if (score >= 90){
+       console.log(`Score: ${score} -> Grade: A`);
+    } else if(score >= 80 && score <= 89){
+        console.log(`Score: ${score} -> Grade: B`);
+    } else if(score >=70 && score <= 79){
+        console.log(`Score: ${score} -> Grade: C`);
+    } else if(score >= 60 && score <= 69){
+        console.log(`Score: ${score} -> Grade: D`);
+
+    } else{
+        console.log(`Score: ${score} -> Grade: F`);
+    }
+
+}
+
+getGrade(89);
+console.log("")
+
+
+
+
+// Discount calculator
+
+
+// function calculatePrice(price, customerType, isFirstPurchase){
+//     let student = 0.1;
+//     let senior = 0.15;
+//     let employee = 0.2;
+    
+    
+//     if (customerType === "student" && isFirstPurchase){
+//         discount = student + 0.05;
+
+//     } else{
+//         discount = student;
+//     }
+
+//     if (customerType === "senior" && isFirstPurchase){
+//         discount = senior + 0.05;
+//     } else {
+//         discount = senior;
+//     }
+
+//     if (customerType === "employee" && isFirstPurchase){
+//         discount = employee + 0.05;
+//     } else {
+//         discount = employee;
+//     }
+
+//     let final_price = price - (price * discount);
+//     console.log(`The final price for ${customerType} is ${final_price} after ${discount}% discount `) 
+// }
+
+// calculatePrice(1000, "senior", true)
+
+
+
+
+function calculatePrice(price, customerType, isFirstPurchase) {
+    let discount = 0;
+    
+    if (customerType === "student") {
+        discount = 0.10; 
+    } else if (customerType === "senior") {
+        discount = 0.15; 
+    } else if (customerType === "employee") {
+        discount = 0.20; 
+    }
+    
+    
+    if (isFirstPurchase) {
+        discount += 0.05; 
+    }
+    
+    let finalPrice = price * (1 - discount);
+    return {
+        originalPrice: price,
+        discountPercent: discount * 100,
+        finalPrice: finalPrice.toFixed(2)
+    };
+}
+
+let result = calculatePrice(100, "student", true)
+console.log("Discount calculation")
+console.log(`startPrice: ${result.originalPrice}`)
+console.log(`Discount: ${result.discountPercent}%`)
+console.log(`Discounted Price: ${result.finalPrice}`)
+
+console.log("")
+// Weather ADVISOR
+
+function weatherAdvice(temperature, isRaining){
+    if (temperature< 32 && isRaining){
+        console.log("Freezing rain! Stay inside!")
+    }
+    else if(temperature < 32){
+        console.log("Very cold, wear a heavy coat.")
+    } else if(temperature >= 32 && temperature <= 60){
+        console.log("Chilly, bring a jacket.")
+    } else if(temperature >= 60 && temperature <= 80){
+        console.log("Nice weather!")
+    } else{
+        console.log("It's hot, stay hydrated!")
+    }
+
+    advice = isRaining ? "Bring an Umbrella" : "No umbrella needed"
+    console.log(advice);
+}
+
+weatherAdvice(82, false)
+console.log("")
+
+// ATM Simulation
+
+function atm(balance, action, amount){
+    if (action  === "withdraw"){
+        if (amount > 500){
+            console.log("Withdrawals of amounts over 500 are not allowed!!!")
+        }
+        else if (balance >= amount){
+            balance = balance - amount;
+            message = `Withdrawal successful. Your balance is now: ${balance}`
+            console.log(message)
+            
+            
+        } else{
+            message = `Insufficient funds!!!`
+            console.log(message)
+            
+        }
+}
+    else if (action  === "deposit"){
+        if (amount > 0){
+            balance = balance + amount;
+        message = `Deposit successful. Your balance is now: ${balance}`
+            console.log(message)
+
+        } else{
+            message = `Enter a valid amount!!!`
+            console.log(message)
+            
+        }
+
+    }
+}
+
+atm(1000, "withdraw", 600)
