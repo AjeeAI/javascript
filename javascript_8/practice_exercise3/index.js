422669a8bb7de0c46bec1e02596ea95ab399634f
Adesoji Ajijolaoluwa <ajeeaidev@gmail.com>
2025-10-04 21:37:28 +0100

    Final version of javascript 8 exercises

--- javascript_8/practice_exercise3/index.js
+++ javascript_8/practice_exercise3/index.js
@@ -1,10 +1,8 @@
 
-
-
 let saveTimer;
-
-let save_button = document.getElementById("delete");
+let lastSavedContent = "";
 let getContent = document.getElementById("input-field");
+let message = document.getElementById("message");
 function save(){
     saveTimer = setInterval(()=> {
 
@@ -19,35 +17,49 @@ function save(){
         let time = `${hours}hrs:${minutes}min:${seconds}secs`;
         
         let content = getContent.value.trim();
+         let loaded = localStorage.getItem("savedContent");
+        let restored = loaded? JSON.parse(loaded): {text:""};
+        let text = restored.text;
+        lastSavedContent = text;
         let data = {
             text: content,
             timestamp: time
         }
-        
-        if (content !== ""){
+        console.log(data);
+        console.log("new content: ",content);
+        console.log("Last saved content: ",lastSavedContent);
+
+        if (content !== "" && content !== lastSavedContent){
         localStorage.setItem("savedContent", JSON.stringify(data));
         // alert("Content saved after 5 seconds");
     } else {
+        message.textContent = "Nothing was typed or no change was made.";
+    message.style.color = "red";
+        setTimeout(()=> {
+            message.textContent = "";
+        }, 3000)
         // alert("Nothing was typed!")
     }}, 5000);
     
 }
 
 function load(){
-        clearInterval
+       
         let loaded = localStorage.getItem("savedContent");
         let data = JSON.parse(loaded)
         let text = data.text;
-        getContent.textContent = text;
+        getContent.value = text;
      
 }
 
-// function clear(){
-//     localStorage.clear("savedContent");
-// }
-// save_button.addEventListener("click", clear());
-// save();
-setTimeout(save, 5000);
-setTimeout(load, 6000);
+
+let loaded = localStorage.getItem("savedContent");
+let data = JSON.parse(loaded)
+let text = data.text;
+
+// setTimeout(save, 5000);
+save();
+load();
+// setTimeout(load, 6000);
 
 // window.addEventListener("load", load());
